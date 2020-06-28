4b17c7fb8cda428608030d367571fcc3c65b4da1
swapnilmisal <swapnil.misal@gmail.com>
2020-06-28 13:31:24 +0530

    Updates

---
+++ precache-manifest.1b02a0471e546f91f96ad0121be29227.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014f342f8eb996e7c30b3c0b78b66e9c",
+    "url": "/react-assignment2/index.html"
+  },
+  {
+    "revision": "773576c8e5b7f4327420",
+    "url": "/react-assignment2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3b6c50ae285fb0d7ecdb",
+    "url": "/react-assignment2/static/js/2.4a2d8096.chunk.js"
+  },
+  {
+    "revision": "773576c8e5b7f4327420",
+    "url": "/react-assignment2/static/js/main.5665835d.chunk.js"
+  },
+  {
+    "revision": "a2f42bf32853263517d5",
+    "url": "/react-assignment2/static/js/runtime~main.abb6da22.js"
+  }
+]);
